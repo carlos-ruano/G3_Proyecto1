84d49b50bd5ee1f4c129b671fe080d186aba0f40
AdrianSumagui <adriansumagui@outlook.com>
2022-03-04 18:40:01 +0100

    Métodos de ColeccionJuegos()

--- src/G3_Proyecto1/datos/ColeccionJuegosImpl.java
+++ src/G3_Proyecto1/datos/ColeccionJuegosImpl.java
@@ -1,5 +1,70 @@
 package G3_Proyecto1.datos;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import G3_Proyecto1.modelos.Juego;
+
+
 public class ColeccionJuegosImpl implements ColeccionJuegos {
+	
+	private Set <Juego> listado = new HashSet <Juego> ();
+	
+	public ColeccionJuegosImpl() {
+		super();
+	}
+
+	public ColeccionJuegosImpl(Set<Juego> listado) {
+		super();
+		this.listado = listado;
+	}
+
+	public Set<Juego> getListado() {
+		return listado;
+	}
+
+	public void setListado(Set<Juego> listado) {
+		this.listado = listado;
+	}
+
+	@Override
+	public String toString() {
+		return "ColeccionJuegosImpl [listado=" + listado + "]";
+	}
+	
+
+	@Override
+	public void addJuego(Juego juego) {
+		
+		listado.add(juego);
+		
+	}
+
+
+	@Override
+	public void deleteJuego(Juego juego) {
+		
+		// Mejorable con un try catch.
+		
+		if (!listado.isEmpty()) {
+			
+			listado.remove(juego);
+			
+		} else {
+			
+			System.out.println("Este juego no existe o la lista esta vacía."); 
+			
+		}
+
+		
+	}
+
+	@Override
+	public Juego getByRank(int rank) {
+		
+		return null;
+	} 
+	
+	
 
 }
