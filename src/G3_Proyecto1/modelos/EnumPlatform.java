d53affa45de3efeca065679987a926e25c18c18f
alumno <alumno@WIN-QNUDKMEQ1RU>
2022-03-04 14:44:33 +0100

    Creo clase Juego con sus Getters and stters y los Enumerados

---
+++ src/G3_Proyecto1/modelos/EnumPlatform.java
@@ -0,0 +1,8 @@
+package G3_Proyecto1.modelos;
+
+public enum EnumPlatform {
+
+	// Mirar el 3DO y 3DS, no me deja y pongo TRES_DO y TRES_DS
+	ATARI2600, TRES_DO, TRES_DS, DC, DS, GB, GBA, GC, GEN, GG, N64, NES, NG, PC,PCFX, PS, 
+	PS2, PS3, PS4, PSP, PSV, SAT, SCD, SNES, TG16, WII, WIIU, WS, X360, XB, XONE
+};
