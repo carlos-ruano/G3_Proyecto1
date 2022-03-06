3807faf5f6e19c0655ad4ff9f2cc8aa7bc864b50
alumno <alumno@WIN-QNUDKMEQ1RU>
2022-03-06 17:26:31 +0100

    Formatear todas las clases

---
+++ src/hola/Lucasteam.java
@@ -0,0 +1,67 @@
+package G3_Proyecto1.control;
+
+import G3_Proyecto1.servicios.LucasteamService;
+import G3_Proyecto1.servicios.LucasteamServiceImpl;
+import G3_Proyecto1.utilidades.Datos;
+import G3_Proyecto1.gui.Menu;
+public class Lucasteam {
+	
+	private LucasteamService services = new LucasteamServiceImpl();
+
+    public void abrirLucasteam() {
+        boolean seguir = true;
+        do {
+            Menu.mostrarMenu();
+            seguir = this.seleccionOpciones();
+        } while (seguir);
+        System.out.println("   --- Fin de la sesion ---");
+    }
+
+    public boolean seleccionOpciones() {
+
+        boolean continuar = true;
+
+        try {
+            switch (Datos.recogeInt()) {
+                case 1:
+                    //IMPORTAR LISTADO DE JUEGOS
+                    services.importarListado();
+                    break;
+
+                case 2:
+                    //LISTAR TODOS LOS JUEGOS
+                    services.mostrarListado();
+                    break;
+
+                case 3:
+                    //FILTRAR POR GENERO PLATAFORMA (servicio ya te lo da)
+                    services.filtrarByGenrePlatform();
+                    break;
+                    
+                case 4:
+                    //FILTRAR POR GENERO (servicio va a pedir el genero)
+                    services.filtrarByGenre();
+                    break;
+
+                case 5:
+                    //DAR DE ALTA UN JUEGO
+                    services.addJuego();
+                    break;
+
+                case 0:
+                    continuar = salir();
+                    break;
+            }
+        } catch (Exception e) {
+            System.out.println("error: " + e.toString());
+        }
+        return continuar;
+    }
+
+    private boolean salir() throws Exception {
+        String sino = Datos.recogeString("   ¿Está seguro?(S/N)");
+        return (sino.toUpperCase().charAt(0) != 'S');
+    }
+
+
+}
---
+++ src/hola/Main.java
@@ -0,0 +1,11 @@
+package G3_Proyecto1.control;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Lucasteam().abrirLucasteam();
+
+	}
+
+}
